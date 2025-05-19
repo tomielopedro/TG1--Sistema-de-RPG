aba4bfe8c946997ea5a0c5a3062189c651cdbb95
Carolina Pedone Barroco <carolpbarroco@gmail.com>
2025-05-19 09:01:15 -0300

    Detalhes corrigidos após análise (Partida)

--- App/RPG/Partida.py
+++ App/RPG/Partida.py
@@ -6,14 +6,12 @@ import uuid
 @dataclass
 class Partida:
     """
-    Classe que representa uma partida (batalha) realizada entre personagens em uma arena.
-
+    Classe que representa uma PARTIDA (composta por uma batalha) realizada entre personagens em uma ARENA.
     Atributos:
-        id (str): Identificador único da partida. Pode ser gerado com `uuid.uuid4().hex`.
+        id (str): Identificador único da partida. Gerado com `uuid.uuid4().hex`.
         descricao (str): Descrição textual da partida (ex: "Batalha Total", "Duelo 1x1").
-        logs (List[LogCombate]): Lista de eventos de combate (ataques, habilidades, etc.) ocorridos durante a partida.
-        vencedor (Optional[str]): Nome do personagem vencedor da partida (caso definido).
-
+        logs (List[LogCombate]): Lista de eventos de combate, logs de (ataques, habilidades, etc.) ocorridos durante a partida.
+        vencedor (Optional[str]): Nome do personagem vencedor da partida, dado atualizado ao ser definido.
     Métodos:
         adicionar_log(log: LogCombate): Adiciona um novo registro de evento à lista de logs da partida.
     """
@@ -26,7 +24,6 @@ class Partida:
     def adicionar_log(self, log: LogCombate):
         """
         Adiciona um log de combate à lista de eventos da partida.
-
         Args:
             log (LogCombate): Instância contendo os detalhes do evento de combate a ser registrado.
         """
